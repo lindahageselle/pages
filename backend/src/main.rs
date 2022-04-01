0661ef429c2a0982c68c52c4734e564fc9adfbda
lindahageselle <72336824+lindahageselle@users.noreply.github.com>
2022-04-01 12:03:59 +0200

    Changed some function names

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -53,7 +53,7 @@ async fn aragog_get_all(conn: &DatabaseConnection) -> Vec<DatabaseRecord<test_co
     records
 }
 
-async fn aragog_create(conn: &DatabaseConnection) {
+async fn aragog_create_linda(conn: &DatabaseConnection) {
     // Define a document
     let mut user = test_collection {
         username: String::from("linda"),
@@ -63,7 +63,7 @@ async fn aragog_create(conn: &DatabaseConnection) {
     let mut user_record = DatabaseRecord::create(user, conn).await.unwrap();
 }
 
-async fn aragog_update(conn: &DatabaseConnection) {
+async fn aragog_update_linda_to_anna(conn: &DatabaseConnection) {
     let query = test_collection::query().filter(Filter::new(
         Comparison::field("username").equals_str("linda"),
     ));
@@ -76,7 +76,7 @@ async fn aragog_update(conn: &DatabaseConnection) {
     user_record.save(conn).await.unwrap();
 }
 
-async fn aragog_delete(conn: &DatabaseConnection) {
+async fn aragog_delete_anna(conn: &DatabaseConnection) {
     let query = test_collection::query().filter(Filter::new(
         Comparison::field("username").equals_str("Anna"),
     ));
@@ -92,14 +92,14 @@ async fn aragog_delete(conn: &DatabaseConnection) {
 async fn main() -> std::io::Result<()> {
     let connection = aragog_connect().await;
 
+    // Creates new entry with username "linda"
+    // aragog_create_linda(&connection).await;
+
     // updates entry with username "linda" to username "Anna"
-    // aragog_update(&connection).await;
+    // aragog_update_linda_to_anna(&connection).await;
 
     // Deletes entry with username "Anna"
-    // aragog_delete(&connection).await;
-
-    // Creates new entry with username "linda"
-    // aragog_create(&connection).await;
+    // aragog_delete_anna(&connection).await;
 
     // Gets all entries as a QueryResult
     let records = aragog_get_all_query_result(&connection).await;
